487f37a8136c8edec2d07b6f1877c05e284faec5
wang.baozhi <286790238@qq.com>
2017-03-09 19:03:03 +0800

    update parameter

--- src/main/scala/cn/whaley/datawarehouse/dimension/constant/Constants.scala
+++ src/main/scala/cn/whaley/datawarehouse/dimension/constant/Constants.scala
@@ -8,5 +8,6 @@ object Constants {
   val DIMENSION_HDFS_BASE_PATH="/data_warehouse/dw_dimensions"
   val DIMENSION_HDFS_BASE_PATH_BACKUP="/data_warehouse/dw_dimensions/backup"
   val DIMENSION_HDFS_BASE_PATH_TMP="/data_warehouse/dw_dimensions/tmp"
+  val THRESHOLD_VALUE=128000
 
 }
