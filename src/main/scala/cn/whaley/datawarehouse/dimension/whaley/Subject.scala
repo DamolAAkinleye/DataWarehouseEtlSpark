7e6e55f8669c8fabd995d13b51e9e4c52a6cf6fc
chen.zhiwei@whaley.cn <Czw941117>
2017-03-14 19:52:00 +0800

    数据仓库维度表微鲸端

--- src/main/scala/cn/whaley/datawarehouse/dimension/whaley/Subject.scala
+++ src/main/scala/cn/whaley/datawarehouse/dimension/whaley/Subject.scala
@@ -2,31 +2,72 @@ package cn.whaley.datawarehouse.dimension.whaley
 
 import cn.whaley.datawarehouse.dimension.DimensionBase
 import cn.whaley.datawarehouse.dimension.constant.SourceType._
-import cn.whaley.datawarehouse.dimension.whaley.Singer.sourceDb
 import cn.whaley.datawarehouse.util.MysqlDB
+import org.apache.spark.sql.DataFrame
 
 
 /**
-  * Created by czw on 17/3/14.
+  * Created by czw on 3/13/17.
   *
-  * 微鲸专题维度表
+  * 微鲸端节目专题维度表
   */
 object Subject extends DimensionBase {
+
+  dimensionName = "dim_whaley_subject"
+
   columns.skName = "subject_sk"
+
   columns.primaryKeys = List("subject_code")
+
   columns.trackingColumns = List()
-  columns.otherColumns = List("subject_name", "content_type")
+
+  columns.otherColumns = List(
+
+    "subject_name",
+    "subject_title",
+    "subject_content_type",
+    "subject_content_type_name",
+    "subject_create_time",
+    "subject_publish_time"
+
+  )
 
   readSourceType = jdbc
 
-  //维度表的字段对应源数据的获取方式
-  sourceColumnMap = Map(
-    "subject_name" -> "title",
-    "content_type" -> "item_contentType"
-  )
+  sourceDb = MysqlDB.whaleyCms("mtv_subject", "ID", 1, 4000, 5)
 
-  sourceFilterWhere = "subject_code is not null and subject_code <> ''"
-  sourceDb = MysqlDB.whaleyTvService("mtv_subjectItem", "subject_code", 1, 1500000, 10)
 
-  dimensionName = "dim_whaley_subject"
+  /**
+    * 处理原数据的自定义的方法
+    * 默认可以通过配置实现，如果需要自定义处理逻辑，可以再在子类中重载实现
+    *
+    * @param sourceDf
+    * @return
+    */
+  override def filterSource(sourceDf: DataFrame): DataFrame = {
+
+    val sq = sqlContext
+    import sq.implicits._
+    import org.apache.spark.sql.functions._
+
+    val contentTypeDb = MysqlDB.whaleyCms("mtv_content_type", "id", 1, 100, 1)
+
+    val contentTypeDf = sqlContext.read.format("jdbc").options(contentTypeDb).load()
+      .select($"code", $"name")
+
+    sourceDf.filter($"code".isNotNull && $"code" != "")
+      .withColumn("codev", regexp_extract($"code", "[a-z]*", 0)).as("s")
+
+      .join(contentTypeDf.as("c"), $"s.codev" === $"c.code", "left_outer")
+      .select(
+        $"s.code".as(columns.primaryKeys(0)),
+        $"s.name".as(columns.otherColumns(0)),
+        $"s.title".as(columns.otherColumns(1)),
+        $"c.code".as(columns.otherColumns(2)),
+        $"c.name".as(columns.otherColumns(3)),
+        $"s.create_time".cast("timestamp").as(columns.otherColumns(4)),
+        $"s.publish_time".cast("timestamp").as(columns.otherColumns(5))
+      )
+
+  }
 }
