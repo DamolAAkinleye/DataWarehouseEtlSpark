f2821ffe19e13c0f5cde6d2fffc59a6b14a5207d
李拓 <li.tuo@whaley.cn>
2017-03-15 22:08:18 +0800

    reformat

--- src/main/scala/cn/whaley/datawarehouse/dimension/whaley/Account.scala
+++ src/main/scala/cn/whaley/datawarehouse/dimension/whaley/Account.scala
@@ -1,7 +1,6 @@
 package cn.whaley.datawarehouse.dimension.whaley
 
 import cn.whaley.datawarehouse.dimension.DimensionBase
-import cn.whaley.datawarehouse.dimension.constant.SourceType._
 import cn.whaley.datawarehouse.util.MysqlDB
 import org.apache.spark.sql.DataFrame
 
@@ -15,12 +14,12 @@ object Account extends DimensionBase {
   columns.skName = "account_sk"
   columns.primaryKeys = List("account_id")
   columns.trackingColumns = List()
-  columns.otherColumns = List("uid","user_name","password","email","reg_ip",
-    "reg_time","last_login_ip","last_login_time","status",
-    "union_id","mobile","email_pwd","register_from","gender","device","birthday","update_time")
+  columns.otherColumns = List("uid", "user_name", "password", "email", "reg_ip",
+    "reg_time", "last_login_ip", "last_login_time", "status",
+    "union_id", "mobile", "email_pwd", "register_from", "gender", "device", "birthday", "update_time")
 
 
-  sourceDb = MysqlDB.whaleyAccount("bbs_ucenter_helios_members","uid",1,1000000000,10)
+  sourceDb = MysqlDB.whaleyAccount("bbs_ucenter_helios_members", "uid", 1, 1000000000, 10)
 
   dimensionName = "dim_whaley_account"
 
@@ -34,15 +33,15 @@ object Account extends DimensionBase {
       "from `bbs_ucenter_helios_members` where uid is not null and heliosid is not null and heliosid <> '' ").registerTempTable("userMembers")
 
 
-    val userMemberDb = MysqlDB.whaleyAccount("bbs_ucenter_helios_memberfields","uid",1,1000000000,10)
+    val userMemberDb = MysqlDB.whaleyAccount("bbs_ucenter_helios_memberfields", "uid", 1, 1000000000, 10)
     sqlContext.read.format("jdbc").options(userMemberDb).load().registerTempTable("bbs_ucenter_helios_memberfields")
     sqlContext.sql("select uid,gender,device,birthday,from_unixtime(update_time) as update_time from `bbs_ucenter_helios_memberfields`" +
       " where uid is not null ").registerTempTable("userMeberfields")
 
-    sqlContext.sql("select a.uid as uid, a.heliosid as account_id ,a.username as user_name,"+
+    sqlContext.sql("select a.uid as uid, a.heliosid as account_id ,a.username as user_name," +
       " a.password as password, a.email as email,a.regip as reg_ip,a.reg_time as reg_time, a.lastloginip as last_login_ip, " +
       "a.last_login_time as last_login_time,a.status as status,a.unionid as union_id,a.mobile as mobile,a.emailpwd as email_pwd," +
-      "a.registerfrom as register_from,b.gender as gender ,b.device as device,b.birthday as birthday,b.update_time as update_time"+
+      "a.registerfrom as register_from,b.gender as gender ,b.device as device,b.birthday as birthday,b.update_time as update_time" +
       " from userMembers a left join userMeberfields b on a.uid = b.uid")
 
   }
--- src/main/scala/cn/whaley/datawarehouse/dimension/whaley/ProductSN.scala
+++ src/main/scala/cn/whaley/datawarehouse/dimension/whaley/ProductSN.scala
@@ -1,8 +1,7 @@
 package cn.whaley.datawarehouse.dimension.whaley
 
-import java.text.SimpleDateFormat
-import java.util.Calendar
 import java.lang.{Long => JLong}
+
 import cn.whaley.datawarehouse.dimension.DimensionBase
 import cn.whaley.datawarehouse.util.MysqlDB
 import org.apache.spark.sql.DataFrame
@@ -17,7 +16,7 @@ object ProductSN extends DimensionBase {
   columns.skName = "product_sn_sk"
   columns.primaryKeys = List("product_sn")
   columns.trackingColumns = List()
-  columns.otherColumns = List("product_line", "product_model","user_id","rom_version","mac","open_time","wifi_mac","ip", "vip_type","country", "area","province", "city", "isp", "city_level")
+  columns.otherColumns = List("product_line", "product_model", "user_id", "rom_version", "mac", "open_time", "wifi_mac", "ip", "vip_type", "country", "area", "province", "city", "isp", "city_level")
 
 
   sourceDb = MysqlDB.whaleyTerminalMember
@@ -33,20 +32,20 @@ object ProductSN extends DimensionBase {
     //得出用户的信息，status表示用户是否有效，activate_status表示用户是否激活
     //限制id != 1339211 是因为有一条重复数据先去掉（此处为临时解决方案）
     sqlContext.sql("select  serial_number, service_id,rom_version, mac, " +
-      "open_time, wifi_mac,current_ip"+
+      "open_time, wifi_mac,current_ip" +
       s" from mtv_terminal where status =1 and activate_status =1 and serial_number not like 'XX%' and id !=1339211 and serial_number is not null and serial_number <> ''").registerTempTable("userinfo")
 
     //获取用户机型信息
-   val productModulInfo = MysqlDB.dwDimensionDb("whaley_sn_to_productmodel")
+    val productModulInfo = MysqlDB.dwDimensionDb("whaley_sn_to_productmodel")
     sqlContext.read.format("jdbc").options(productModulInfo).load().registerTempTable("whaley_sn_to_productmodel")
     sqlContext.sql("select serial_number , product_model  from whaley_sn_to_productmodel ").registerTempTable("productmodel")
 
-    sqlContext.udf.register("snToProductLine",snToProductLine _)
-    sqlContext.udf.register("getSNtwo",getSNtwo _)
+    sqlContext.udf.register("snToProductLine", snToProductLine _)
+    sqlContext.udf.register("getSNtwo", getSNtwo _)
 
     //将用户机型信息加入用户信息中存为中间表“product”
-    sqlContext.sql("select a.serial_number as product_sn, snToProductLine(a.serial_number) as product_line ,b.product_model as product_model,"+
-      " a.service_id as user_id, a.rom_version as rom_version,a.mac as mac,a.open_time as open_time, a.wifi_mac as wifi_mac, a.current_ip as ip"+
+    sqlContext.sql("select a.serial_number as product_sn, snToProductLine(a.serial_number) as product_line ,b.product_model as product_model," +
+      " a.service_id as user_id, a.rom_version as rom_version,a.mac as mac,a.open_time as open_time, a.wifi_mac as wifi_mac, a.current_ip as ip" +
       " from userinfo a left join productmodel b on getSNtwo(a.serial_number) = b.serial_number").registerTempTable("product")
 
 
@@ -54,26 +53,26 @@ object ProductSN extends DimensionBase {
     val vipTypeInfo = MysqlDB.whaleyDolphin("dolphin_club_authority", "id", 1, 100000000, 100)
 
     sqlContext.read.format("jdbc").options(vipTypeInfo).load().registerTempTable("dolphin_club_authority")
-     sqlContext.sql("select distinct sn , 'vip' as  vip  from dolphin_club_authority where sn not like 'XX%'").registerTempTable("vipType")
+    sqlContext.sql("select distinct sn , 'vip' as  vip  from dolphin_club_authority where sn not like 'XX%'").registerTempTable("vipType")
 
 
     //将用户会员信息加入用户信息中作为中间表。表明为"userVipInfo"
-    sqlContext.sql("select a.product_sn as product_sn, a.product_line as product_line ,a.product_model as product_model,"+
-                   " a.user_id as user_id, a.rom_version as rom_version,a.mac as mac,a.open_time as open_time, a.wifi_mac as wifi_mac, a.ip as ip,"+
-                   "case b.vip when 'vip' then 'vip' else '未知' end  as vip_type from product a left join vipType b on a.product_sn = b.sn")
-                 .registerTempTable("userVipInfo")
+    sqlContext.sql("select a.product_sn as product_sn, a.product_line as product_line ,a.product_model as product_model," +
+      " a.user_id as user_id, a.rom_version as rom_version,a.mac as mac,a.open_time as open_time, a.wifi_mac as wifi_mac, a.ip as ip," +
+      "case b.vip when 'vip' then 'vip' else '未知' end  as vip_type from product a left join vipType b on a.product_sn = b.sn")
+      .registerTempTable("userVipInfo")
 
     //获取用户的城市信息
 
-    sqlContext.udf.register("getFirstIpInfo",getFirstIpInfo _)
-    sqlContext.udf.register("getSecondIpInfo",getSecondIpInfo _)
-    sqlContext.udf.register("getThirdIpInfo",getThirdIpInfo _)
+    sqlContext.udf.register("getFirstIpInfo", getFirstIpInfo _)
+    sqlContext.udf.register("getSecondIpInfo", getSecondIpInfo _)
+    sqlContext.udf.register("getThirdIpInfo", getThirdIpInfo _)
 
     sqlContext.read.parquet("/data_warehouse/dw_dimensions/dim_web_location").registerTempTable("log_data")
     sqlContext.sql("select ip_section_1,ip_section_2,ip_section_3,country,area,province,city,district,isp,city_level,dim_invalid_time from log_data").registerTempTable("countryInfo")
 
-    sqlContext.sql("select a.product_sn as product_sn, a.product_line as product_line ,a.product_model as product_model,"+
-      " a.user_id as user_id, a.rom_version as rom_version,a.mac as mac,a.open_time as open_time, a.wifi_mac as wifi_mac, a.ip as ip,"+
+    sqlContext.sql("select a.product_sn as product_sn, a.product_line as product_line ,a.product_model as product_model," +
+      " a.user_id as user_id, a.rom_version as rom_version,a.mac as mac,a.open_time as open_time, a.wifi_mac as wifi_mac, a.ip as ip," +
       "a.vip_type  as vip_type ,case when b.country = '' then '未知' when  b.country is null  then '未知' else b.country end  as country," +
       "case when b.area = '' then '未知' when b.area is null then '未知' else b.area end  as area," +
       "case when b.province = '' then '未知' when b.province is null then '未知' else b.province end as province," +
@@ -87,12 +86,12 @@ object ProductSN extends DimensionBase {
 
   }
 
-  def snToProductLine(productSN:String) = {
-    if(productSN == null || productSN == ""){
+  def snToProductLine(productSN: String) = {
+    if (productSN == null || productSN == "") {
       "helios"
-    } else if(productSN.startsWith("P")){
+    } else if (productSN.startsWith("P")) {
       "orca"
-    }else "helios"
+    } else "helios"
   }
 
 
@@ -112,33 +111,33 @@ object ProductSN extends DimensionBase {
     } else null
   }
 
-  def getFirstIpInfo(ip:String) = {
+  def getFirstIpInfo(ip: String) = {
     var firstip = -1
-    if(ip != null){
+    if (ip != null) {
       val ipinfo = ip.split("\\.")
-      if(ipinfo.length >=3) {
-          firstip = ipinfo(0).trim().toInt
-       }
+      if (ipinfo.length >= 3) {
+        firstip = ipinfo(0).trim().toInt
+      }
     }
     firstip
   }
 
-  def getSecondIpInfo(ip:String) = {
+  def getSecondIpInfo(ip: String) = {
     var secondip = -1
-    if(ip != null){
+    if (ip != null) {
       val ipinfo = ip.split("\\.")
-      if(ipinfo.length >=3) {
+      if (ipinfo.length >= 3) {
         secondip = ipinfo(1).trim().toInt
       }
     }
     secondip
   }
 
-  def getThirdIpInfo(ip:String) = {
+  def getThirdIpInfo(ip: String) = {
     var thirdip = -1
-    if(ip != null){
+    if (ip != null) {
       val ipinfo = ip.split("\\.")
-      if(ipinfo.length >=3) {
+      if (ipinfo.length >= 3) {
         thirdip = ipinfo(2).trim().toInt
       }
     }
