dd88cd5a63d4135a9de2973a52038accb3174d2b
李拓 <li.tuo@whaley.cn>
2017-03-15 22:10:24 +0800

    fix

--- src/main/scala/cn/whaley/datawarehouse/util/MysqlDB.scala
+++ src/main/scala/cn/whaley/datawarehouse/util/MysqlDB.scala
@@ -85,7 +85,7 @@ object MysqlDB {
 //      "numPartitions" -> numPartitions.toString)
 //  }
 
-  def whaleyAvccount(table: String, partitionColumn: String, lowerBound: Long, upperBound: Long, numPartitions:Int ) = {
+  def whaleyAccount(table: String, partitionColumn: String, lowerBound: Long, upperBound: Long, numPartitions:Int ) = {
     Map("url" -> "jdbc:mysql://bigdata-extsvr-db_whaley_ucenter:3306/ucenter?useUnicode=true&characterEncoding=utf-8&autoReconnect=true",
       "dbtable" -> table,
       "driver" -> "com.mysql.jdbc.Driver",
