4b23b152f962c2c5897f6f930732187ac56655b7
李拓 <li.tuo@whaley.cn>
2017-03-31 16:28:42 +0800

    update

---
+++ src/main/scala/cn/whaley/datawarehouse/dimension/moretv/LauncherEntrance.scala
@@ -0,0 +1,32 @@
+package cn.whaley.datawarehouse.dimension.moretv
+
+import cn.whaley.datawarehouse.dimension.DimensionBase
+import cn.whaley.datawarehouse.dimension.constant.SourceType.jdbc
+import cn.whaley.datawarehouse.util.MysqlDB
+
+/**
+  * Created by Tony on 17/3/31.
+  */
+object LauncherEntrance extends DimensionBase {
+
+  columns.skName = "launcher_entrance_sk"
+  columns.primaryKeys = List("launcher_entrance_id")
+  columns.trackingColumns = List()
+  columns.allColumns = List(
+    "launcher_entrance_id",
+    "area_code",
+    "area_name",
+    "location_code",
+    "location_name")
+
+  readSourceType = jdbc
+
+  //维度表的字段对应源数据的获取方式
+  sourceColumnMap = Map(
+    "launcher_entrance_id" -> "id"
+  )
+
+  sourceDb = MysqlDB.dwDimensionDb("moretv_launcher_entrance")
+
+  dimensionName = "dim_medusa_launcher_entrance"
+}
---
+++ src/main/scala/cn/whaley/datawarehouse/dimension/moretv/PageEntrance.scala
@@ -0,0 +1,35 @@
+package cn.whaley.datawarehouse.dimension.moretv
+
+import cn.whaley.datawarehouse.dimension.DimensionBase
+import cn.whaley.datawarehouse.dimension.constant.SourceType.jdbc
+import cn.whaley.datawarehouse.util.MysqlDB
+
+/**
+  * Created by Tony on 17/3/31.
+  */
+object PageEntrance extends DimensionBase {
+
+  columns.skName = "page_entrance_sk"
+  columns.primaryKeys = List("page_entrance_id")
+  columns.trackingColumns = List()
+  columns.allColumns = List(
+    "page_entrance_id",
+    "page_code",
+    "page_name",
+    "area_code",
+    "area_name",
+    "location_code",
+    "location_name")
+
+  readSourceType = jdbc
+
+  //维度表的字段对应源数据的获取方式
+  sourceColumnMap = Map(
+    "page_entrance_id" -> "id"
+  )
+
+
+  sourceDb = MysqlDB.dwDimensionDb("moretv_page_entrance")
+
+  dimensionName = "dim_medusa_page_entrance"
+}
