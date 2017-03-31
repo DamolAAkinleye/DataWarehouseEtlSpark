d8839b82e4e810dd7c6cdbab6ed5c2f05dcf4575
张羽 <zhang.yu01@whaley.cn>
2017-03-31 14:51:55 +0800

    修改微鲸站点树维度表

--- src/main/scala/cn/whaley/datawarehouse/dimension/whaley/SubPath.scala
+++ src/main/scala/cn/whaley/datawarehouse/dimension/whaley/SubPath.scala
@@ -30,7 +30,7 @@ object SubPath extends DimensionBase{
      "subpath_root"->"root"
    )
 
-   sourceFilterWhere = "subpath_id is not null and subpath_status = 1 and root != 0"
+   sourceFilterWhere = "subpath_id is not null and subpath_status = 1 and subpath_root != 0"
    sourceDb = MysqlDB.whaleyCms("mtv_program_site","id",1,4131,10)
 
    dimensionName = "dim_whaley_subpath"
