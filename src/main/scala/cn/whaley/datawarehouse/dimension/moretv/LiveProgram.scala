79b8edc8e60e1a8871574181259ecdbe1a311905
wujiulin <wu.jiulin@whaley.cn>
2017-04-19 19:43:07 +0800

    live program add five columns.

--- src/main/scala/cn/whaley/datawarehouse/dimension/moretv/LiveProgram.scala
+++ src/main/scala/cn/whaley/datawarehouse/dimension/moretv/LiveProgram.scala
@@ -7,6 +7,9 @@ import org.apache.spark.sql.DataFrame
 
 /**
   * Created by witnes on 3/14/17.
+  * update by wu.jiulin on 4/19/17.
+  * 增加五个字段:live_program_compere(直播播主),live_type(一级直播类型),live_type_name(一级直播类型名称),
+  * live_type_2(二级直播类型),live_type_name_2(二级直播类型名称)
   * 直播节目维度表
   */
 object LiveProgram extends DimensionBase {
@@ -21,9 +24,10 @@ object LiveProgram extends DimensionBase {
 
   columns.allColumns = List(
     "live_program_sid", "live_program_title", "live_program_source", "live_program_site", "live_program_create_time",
-    "live_program_publish_time"
+    "live_program_publish_time","live_program_compere","live_type","live_type_name","live_type_2","live_type_name_2"
   )
 
+  debug = false
 
   readSourceType = jdbc
 
@@ -33,17 +37,27 @@ object LiveProgram extends DimensionBase {
 
     val sq = sqlContext
     import sq.implicits._
-    import org.apache.spark.sql.functions._
 
-    sourceDf.filter($"sid".isNotNull).dropDuplicates("sid" :: Nil)
+    val liveTypeDb = MysqlDB.dwDimensionDb("moretv_live_type")
+    val liveTypeDf = sqlContext.read.format("jdbc").options(liveTypeDb).load()
+      .select($"live_type", $"live_type_name", $"live_type_2", $"live_type_name_2")
+
+    val sourceDfFilter = sourceDf.filter($"sid".isNotNull).dropDuplicates("sid" :: Nil)
+    sourceDfFilter.as("b").join(liveTypeDf.as("a"), liveTypeDf("live_type_2")===sourceDfFilter("live_type_2"), "left_outer")
       .select(
-        $"sid".as(columns.primaryKeys(0)),
-        $"title".as(columns.allColumns(1)),
-        $"source".as(columns.allColumns(2)),
-        $"site".as(columns.allColumns(3)),
-        $"create_time".cast("timestamp").as(columns.allColumns(4)),
-        $"publish_time".cast("timestamp").as(columns.allColumns(5))
+        $"b.sid".as(columns.primaryKeys(0)),
+        $"b.title".as(columns.allColumns(1)),
+        $"b.source".as(columns.allColumns(2)),
+        $"b.site".as(columns.allColumns(3)),
+        $"b.create_time".cast("timestamp").as(columns.allColumns(4)),
+        $"b.publish_time".cast("timestamp").as(columns.allColumns(5)),
+        $"b.program_compere".as(columns.allColumns(6)),
+        $"a.live_type".as(columns.allColumns(7)),
+        $"a.live_type_name".as(columns.allColumns(8)),
+        $"a.live_type_2".as(columns.allColumns(9)),
+        $"a.live_type_name_2".as(columns.allColumns(10))
       )
+
   }
 
 
