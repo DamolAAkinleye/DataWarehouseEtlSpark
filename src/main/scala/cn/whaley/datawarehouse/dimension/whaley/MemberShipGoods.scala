18a37cdc631ddb4bbd4ffb472544f3df85c2d6c3
郭浩 <guo.hao@whaley.cn>
2017-04-26 20:44:10 +0800

    修改会员商品表

--- src/main/scala/cn/whaley/datawarehouse/dimension/whaley/MemberShipGoods.scala
+++ src/main/scala/cn/whaley/datawarehouse/dimension/whaley/MemberShipGoods.scala
@@ -15,7 +15,7 @@ object MemberShipGoods extends DimensionBase {
   columns.skName = "membership_goods_sk"
   columns.primaryKeys = List("goods_no")
   columns.trackingColumns = List()
-  columns.allColumns = List("goods_no","goods_name",
+  columns.allColumns = List("goods_no","goods_name","goods_package_type",
     "goods_tag","prime_price","discount","real_price","status","goods_type",
     "goods_title", "is_display","create_time","update_time","publish_time",
     "start_time","end_time")
@@ -31,22 +31,23 @@ object MemberShipGoods extends DimensionBase {
     import sq.implicits._
     import org.apache.spark.sql.functions._
     sourceDf.
-      select(
-        $"goodsNo".as(columns.allColumns(0)),
-        $"goodsName".as(columns.allColumns(1)),
-        $"goodsUseTag".as(columns.allColumns(2)),
-        $"primePrice".as(columns.allColumns(3)),
-        $"discount".cast("double")as(columns.allColumns(4)),
-        $"realPrice".as(columns.allColumns(5)),
-        $"goodsStatus".as(columns.allColumns(6)),
-        $"goodsType".as(columns.allColumns(7)),
-        $"goodsTitle".as(columns.allColumns(8)),
-        $"isDisplay".as(columns.allColumns(9)),
-        $"createTime".cast("timestamp").as(columns.allColumns(10)),
-        $"updateTime".cast("timestamp").as(columns.allColumns(11)),
-        $"publishTime".cast("timestamp").as(columns.allColumns(12)),
-        $"startTime".cast("timestamp").as(columns.allColumns(13)),
-        $"endTime".cast("timestamp").as(columns.allColumns(14))
+      selectExpr(
+        "goodsNo as goods_no ",
+        "goodsName as  goods_name " ,
+        "(case when trim(goodsName) = '白金升级钻石' then '白金升级钻石'  when  trim(goodsName) like '%鲸鲸%' then '鲸鲸会员'  when  trim(goodsName) like '%钻石%' then '钻石会员' when  trim(goodsName) like '%白金%' then '白金会员' else '其他' end ) as goods_package_type " ,
+        "goodsUseTag as goods_tag " ,
+        "primePrice as prime_price" ,
+        "cast(discount as double ) as discount" ,
+        "realPrice as real_price" ,
+        "goodsStatus as status" ,
+        "goodsType as goods_type" ,
+        "goodsTitle as goods_title" ,
+        "isDisplay as is_display" ,
+        "cast(createTime as timestamp ) as create_time " ,
+        "cast(updateTime as timestamp ) as update_time " ,
+        "cast(publishTime as timestamp ) as publish_time " ,
+        "cast(startTime as timestamp ) as start_time " ,
+        "cast(endTime as timestamp ) as end_time "
       )
   }
 
