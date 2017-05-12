86b8fee64a2333eac68406acfb202cbb609b7da8
wujiulin <wu.jiulin@whaley.cn>
2017-05-12 10:00:58 +0800

    update ListCategoryUtils file.

--- src/main/scala/cn/whaley/datawarehouse/fact/util/FilterCategoryUtils.scala
+++ src/main/scala/cn/whaley/datawarehouse/fact/util/FilterCategoryUtils.scala
@@ -1,20 +1,24 @@
 package cn.whaley.datawarehouse.fact.util
 
+import cn.whaley.datawarehouse.common.{DimensionColumn, DimensionJoinCondition}
 import cn.whaley.datawarehouse.global.LogConfig
 /**
   * Created by michael on 2017/5/2.
+  * Updated by wujiulin on 2017/5/11.
+  *  1.解析出content_type
+  *  2.获得retrieval_sk
   */
 object FilterCategoryUtils extends LogConfig{
   private val regex_moretv_filter = (".*multi_search-(hot|new|score)-([\\w]+)-([\\w]+)-([\\w]+)[-]?.*").r
   private val regex_moretv_filter_number = (".*multi_search-(hot|new|score)-([\\w]+)-([\\w]+)-([\\d]+-[\\d]+)[-]?.*").r
-  private val regex_medusa_filter = (".*retrieval\\*(hot|new|score)\\*([\\w]+)\\*([\\w]+)\\*(all|qita|[\\d]+[\\*\\d]*)").r
+  private val regex_medusa_filter = (".*(movie|tv|zongyi|jilu|comic|xiqu|hot|mv|kids|sports)-retrieval\\*(hot|new|score)\\*([\\w]+)\\*([\\w]+)\\*(all|qita|[\\d]+[\\*\\d]*)").r
 
   //获取筛选维度【排序方式：最新、最热、得分；标签；地区；年代】
   def getFilterCategory(path: String, index: Int, flag: String): String = {
     var result: String = null
     if (null == path) {
       result = null
-    } else if (index > 4) {
+    } else if (index > 5) {
       result = null
     } else {
       flag match {
@@ -45,11 +49,15 @@ object FilterCategoryUtils extends LogConfig{
     result
   }
 
+  def getFilterCategoryContentType(pathMain: String): String = {
+    getFilterCategory(pathMain,1,MEDUSA)
+  }
+
   def getFilterCategoryFirst(pathMain: String, path: String, flag: String): String = {
     var result: String = null
     flag match {
       case MEDUSA => {
-        result = getFilterCategory(pathMain,1,MEDUSA)
+        result = getFilterCategory(pathMain,2,MEDUSA)
       }
       case MORETV => {
         result = getFilterCategory(path,1,MORETV)
@@ -62,7 +70,7 @@ object FilterCategoryUtils extends LogConfig{
     var result: String = null
     flag match {
       case MEDUSA => {
-        result = getFilterCategory(pathMain,2,MEDUSA)
+        result = getFilterCategory(pathMain,3,MEDUSA)
       }
       case MORETV => {
         result = getFilterCategory(path,2,MORETV)
@@ -75,7 +83,7 @@ object FilterCategoryUtils extends LogConfig{
     var result: String = null
     flag match {
       case MEDUSA => {
-        result = getFilterCategory(pathMain,3,MEDUSA)
+        result = getFilterCategory(pathMain,4,MEDUSA)
       }
       case MORETV => {
         result = getFilterCategory(path,3,MORETV)
@@ -88,13 +96,34 @@ object FilterCategoryUtils extends LogConfig{
     var result: String = null
     flag match {
       case MEDUSA => {
-        result = getFilterCategory(pathMain,4,MEDUSA)
+        result = getFilterCategory(pathMain,5,MEDUSA)
       }
       case MORETV => {
         result = getFilterCategory(path,4,MORETV)
       }
     }
     result
+  }
+
+  /** 筛选维度表，获得retrieval_sk
+    * 事实表中字段                                      维度表中字段
+    * filterCategoryFirst  (udf解析出字段)     对应      sort_type
+    * filterCategorySecond (udf解析出字段)     对应      filter_category_first
+    * filterCategoryThird  (udf解析出字段)     对应      filter_category_second
+    * filterCategoryFourth (udf解析出字段)     对应      filter_category_third
+    * retrievalContentType (udf解析出字段)     对应      content_type
+    */
+  def getRetrievalSK() :DimensionColumn = {
+    new DimensionColumn("dim_medusa_retrieval",
+      List(
+        DimensionJoinCondition(
+          Map("retrievalContentType" -> "content_type","filterCategoryFirst" -> "sort_type",
+            "filterCategorySecond" -> "filter_category_first","filterCategoryThird" -> "filter_category_second",
+          "filterCategoryFourth" -> "filter_category_third"),
+          null, null, null
+        )
+      ),
+      "retrieval_sk")
   }
 
 }
