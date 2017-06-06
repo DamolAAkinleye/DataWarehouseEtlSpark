40d069c9774f6050d262ac5ce3750d56b87a9a09
郭浩 <guo.hao@whaley.cn>
2017-06-06 19:38:10 +0800

    添会员订单添加订单支付方式

--- src/main/scala/cn/whaley/datawarehouse/dimension/whaley/MemberShipAccountOrder.scala
+++ src/main/scala/cn/whaley/datawarehouse/dimension/whaley/MemberShipAccountOrder.scala
@@ -17,7 +17,7 @@ object MemberShipAccountOrder extends DimensionBase {
   columns.trackingColumns = List()
   columns.allColumns = List("membership_order_id","product_sn","membership_account",
     "order_id","order_status","payment_type","order_type","goods_no","prime_price",
-    "payment_amount","pay_method","foreign_key","over_time","create_time")
+    "payment_amount","pay_method","pay_is_yunos","foreign_key","over_time","create_time")
 
   readSourceType = jdbc
 
@@ -27,7 +27,27 @@ object MemberShipAccountOrder extends DimensionBase {
 
   override def filterSource(sourceDf: DataFrame): DataFrame = {
     val sq = sqlContext
+
     import sq.implicits._
+    sourceDf.filter("orderStutas ='1' and substr(sn,2) not in ('XX','XY','XZ','YX','YY','YZ','ZX')")
+        .selectExpr(
+          "id as membership_order_id",
+          "sn as product_sn",
+          "whaleyAccount as membership_account",
+          "whaleyOrder as order_id",
+          "orderStatus as order_status",
+          "paymentType as payment_type",
+          "orderTypeId as order_type",
+          "goodsNo as goods_no",
+          "totalPrice as prime_price",
+          "paymentAmount as payment_amount",
+          "payMethod as pay_method",
+          "(case when payMethod like '%_yun' then '1' else '0' end ) as  pay_is_yunos",
+          "foreignKey as foreign_key",
+          "overTime as over_time",
+          "createTime as create_time"
+        )
+/*
     sourceDf.filter("orderStutas ='1' and substr(sn,2) not in ('XX','XY','XZ','YX','YY','YZ','ZX')")
       .select(
         $"id".as(columns.allColumns(0)),
@@ -44,7 +64,7 @@ object MemberShipAccountOrder extends DimensionBase {
         $"foreignKey".as(columns.allColumns(11)),
         $"overTime".cast("timestamp").as(columns.allColumns(12)),
         $"createTime".cast("timestamp").as(columns.allColumns(13))
-      )
+      )*/
   }
 
 }
