1561408268997d05394c17b60d40ffc779e5454b
huang.hu@whaley.cn <huang.hu@whaley.cn>
2017-06-09 21:17:06 +0800

    利用长连接的方式更改SN维度表的IP

--- src/main/scala/cn/whaley/datawarehouse/dimension/whaley/ProductSN.scala
+++ src/main/scala/cn/whaley/datawarehouse/dimension/whaley/ProductSN.scala
@@ -4,8 +4,11 @@ package cn.whaley.datawarehouse.dimension.whaley
 import cn.whaley.datawarehouse.common.{DimensionColumn, DimensionJoinCondition, UserDefinedColumn}
 import cn.whaley.datawarehouse.dimension.DimensionBase
 import cn.whaley.datawarehouse.util.MysqlDB
+import cn.whaley.sdk.dataexchangeio.DataIO
+import cn.whaley.sdk.utils.DataFrameUtil
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions.udf
+import org.apache.spark.storage.StorageLevel
 
 /**
   * Created by huanghu on 17/3/14.
@@ -42,6 +45,7 @@ object ProductSN extends DimensionBase {
 
     sourceDf.registerTempTable("mtv_terminal")
 
+
     //得出用户的信息，status表示用户是否有效，activate_status表示用户是否激活
     //限制id != 1339211 是因为有一条重复数据先去掉（此处为临时解决方案）
     sqlContext.sql("select  serial_number, service_id,rom_version, wui_version,mac, " +
@@ -49,20 +53,41 @@ object ProductSN extends DimensionBase {
       s" from mtv_terminal where status =1 and activate_status =1 " +
       s"and device_id is not null and serial_number not like 'XX%' " +
       s"and id !=1339211 and serial_number is not null " +
-      s"and serial_number <> ''").registerTempTable("userinfo")
+      s"and serial_number <> ''").registerTempTable("mtv_terminal_info")
+
+
+    //从长连接中找出各SN号对应的IP
+    println("==============1====================")
+
+
+    val aliveInfo = MysqlDB.lcmsAccount
+    sqlContext.read.format("jdbc").options(aliveInfo).load().where("product = 'whaley'").persist(StorageLevel.MEMORY_AND_DISK).registerTempTable("account")
+
+    //选出SN和对应的最后的时间，保证每个SN只对应一个时间
+   sqlContext.sql("select sn ,max(last_time) AS last_time from account where product = 'whaley'  group by sn ").registerTempTable("sn_time")
+
+    //根据SN和时间选择出对应的IP，只限制最新更新的IP
+    sqlContext.sql("select a.sn,b.real_client_ip from  sn_time a left join account b on a.last_time = b.last_time and a.sn = b.sn where b.product = 'whaley'").registerTempTable("account_info")
+
+    //将用户全量表中的IP和长连接中的IP进行比较如果某个SN在长连接和全量表中均存在则以长连接为准，否则以全量表为准
+    sqlContext.sql("select a.serial_number as serial_number,a.service_id as service_id, a.rom_version as rom_version," +
+      "a.wui_version as wui_version,a.mac as mac,a.open_time as open_time,a.wifi_mac as wifi_mac," +
+      " case when b.real_client_ip is null then a.current_ip  when trim(real_client_ip) ='' then a.current_ip  else b.real_client_ip end as ip" +
+      " from mtv_terminal_info a left join account_info b on a.serial_number = b.sn").registerTempTable("user_info")
+
 
     //获取用户机型信息
     val productModulInfo = MysqlDB.dwDimensionDb("whaley_sn_to_productmodel")
-    sqlContext.read.format("jdbc").options(productModulInfo).load().registerTempTable("whaley_sn_to_productmodel")
-    sqlContext.sql("select serial_number , product_model  from whaley_sn_to_productmodel ").registerTempTable("productmodel")
+    sqlContext.read.format("jdbc").options(productModulInfo).load().registerTempTable("whaley_sn_to_product_model")
+    sqlContext.sql("select serial_number , product_model  from whaley_sn_to_product_model ").registerTempTable("product_model")
 
     sqlContext.udf.register("snToProductLine", snToProductLine _)
     sqlContext.udf.register("getSNtwo", getSNtwo _)
 
     //将用户机型信息加入用户信息中存为中间表“product”
     sqlContext.sql("select a.serial_number as product_sn, snToProductLine(a.serial_number) as product_line ,b.product_model as product_model," +
-      " a.service_id as user_id, a.rom_version as rom_version,a.wui_version as wui_version,a.mac as mac,a.open_time as open_time, a.wifi_mac as wifi_mac, a.current_ip as ip" +
-      " from userinfo a left join productmodel b on getSNtwo(a.serial_number) = b.serial_number").registerTempTable("product")
+      " a.service_id as user_id, a.rom_version as rom_version,a.wui_version as wui_version,a.mac as mac,a.open_time as open_time, a.wifi_mac as wifi_mac, a.ip as ip" +
+      " from user_info a left join product_model b on getSNtwo(a.serial_number) = b.serial_number").registerTempTable("product")
 
 
     //获取用户的会员信息
