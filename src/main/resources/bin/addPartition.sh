9be9b60f5abd450b89c73bc4afa110a9ac1eafa9
郭浩 <guo.hao@whaley.cn>
2017-06-21 19:27:39 +0800

    修改addpartition 脚本

--- src/main/resources/bin/addPartition.sh
+++ src/main/resources/bin/addPartition.sh
@@ -1,21 +1,51 @@
 #!/bin/bash
-if [ $# -ne 8 ]; then
-    echo " usage : `basename  $0`  8 个 参数 '--table ${table} --startDate ${startDate} --endDate ${endDate} --hour ${hour} '"
-    exit 1
-fi
+
 
 source /etc/profile
 cd `dirname $0`
 pwd=`pwd`
 echo $pwd
-table=$2
-day_p=$4
-endDate=$6
-hour_p=$8
-echo $day_p
-echo $hour_p
-day_p=`date -d "-1 days "$day_p +%Y%m%d`
+
+ARGS=`getopt -o d:t:s:e:h --long database:,table:,startDate:,endDate:,hour: -- "$@"`
+
+#将规范化后的命令行参数分配至位置参数（$1,$2,...)
+eval set -- "${ARGS}"
+
+while true
+do
+    case "$1" in
+        -d|--database)
+            database=$2;
+            shift 2;;
+		-t|--table)
+            table=$2;
+            shift 2;;
+        -s|--startDate)
+            startDate=$2;
+            shift 2;;
+        -e|--endDate)
+            endDate=$2;
+            shift 2;;
+        -h|--hour)
+            hour=$2;
+            shift 2;;
+        --)
+            shift;
+            break;;
+
+        *)
+            exit 1
+            ;;
+    esac
+done
+
+
+hour_p=$hour
+
+day_p=`date -d "-1 days "$startDate +%Y%m%d`
 endDate=`date -d "-1 days "$endDate +%Y%m%d`
+
+
 echo "table is $table"
 echo "day_p is $day_p"
 echo "endDate is $endDate"
@@ -24,10 +54,14 @@ echo "hour_p is $hour_p"
 while [[ $day_p -le $endDate ]]
  do
   echo "$day_p    ..................."
-  hive -hivevar table=$table -hivevar day_p=$day_p -hivevar hour_p=$hour_p  -e '
+  hive  -hivevar table=$table -hivevar day_p=$day_p -hivevar hour_p=$hour_p  -e '
   use dw_facts;
   alter table ${hivevar:table} add if not exists partition (day_p=${hivevar:day_p},hour_p="${hivevar:hour_p}") location "/data_warehouse/dw_facts/${hivevar:table}/${hivevar:day_p}/${hivevar:hour_p}";
   '
+  if [ $? -ne 0 ];then
+   echo "hive addPartitions  ${day_p} is fail ..."
+   exit 1
+  fi
   day_p=`date -d "+1 days "$day_p +%Y%m%d`
 done
 
