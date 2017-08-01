b396f12b19e94a28a6a831ced70156227c46906f
张羽 <zhang.yu01@whaley.cn>
2017-08-01 12:24:50 +0800

    修改微鲸端资讯奇趣频道的站点树解析逻辑

--- src/main/scala/cn/whaley/datawarehouse/fact/whaley/util/ListCategoryUtils.scala
+++ src/main/scala/cn/whaley/datawarehouse/fact/whaley/util/ListCategoryUtils.scala
@@ -118,8 +118,8 @@ object ListCategoryUtils {
         }
         case "hot" | "interest" => {
           if(paths.length >= 4){
-            categoryCodes(1)= "site_" + paths(2)
-            categoryCodes(2)= paths(4)
+            categoryCodes(1)= "site_" + paths(1)
+            categoryCodes(2)= paths(3)
           }else if(paths.length >= 3){
             categoryCodes(1)= contentType
             categoryCodes(2)= paths(2)
