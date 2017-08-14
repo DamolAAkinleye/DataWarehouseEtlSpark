93850a18dafd11b7df0adbf9d1aecaa8835b9917
HsiaChubby <hsiachubby@gmail.com>
2017-08-14 15:35:52 +0800

    add filter condition

--- src/main/scala/cn/whaley/datawarehouse/dimension/moretv/MVTopic.scala
+++ src/main/scala/cn/whaley/datawarehouse/dimension/moretv/MVTopic.scala
@@ -42,7 +42,7 @@ object MVTopic extends DimensionBase {
   sourceDb = MysqlDB.medusaCms("mtv_mvtopic", "id", 1, 134, 1)
 
   //临时过滤34fhwxac9wtv防止报错
-  sourceFilterWhere = "mv_topic_sid is not null and mv_topic_sid <> '' and mv_status=1"
+  sourceFilterWhere = "mv_topic_sid is not null and mv_topic_sid <> '' and mv_status=1 and mv_topic_sid <> '34fhwxac9wtv'"
 
 
 
