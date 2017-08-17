25efc460c7d8690c3a9d3ae4dabd679fa01962e5
张羽 <zhang.yu01@whaley.cn>
2017-08-17 17:02:11 +0800

    修正播放模型

--- src/main/scala/cn/whaley/datawarehouse/fact/whaley/Play.scala
+++ src/main/scala/cn/whaley/datawarehouse/fact/whaley/Play.scala
@@ -26,8 +26,11 @@ object Play extends FactEtlBase {
       udf(LauncherEntranceUtils.launcherAccessLocationFromPath: (String, String) => String),
       List("path", "linkValue")),
     UserDefinedColumn("udc_launcher_location_index",
-      udf(LauncherEntranceUtils.launcherLocationIndexFromPlay: String => Int),
-      List("recommendLocation")),
+      udf(LauncherEntranceUtils.launcherLocationIndexFromPlay: (String, String) => Int),
+      List("path","recommendLocation")),
+    UserDefinedColumn("udc_launcher_access_area",
+      udf(LauncherEntranceUtils.launcherAccessAreaFromPlayPath: String => String),
+      List("path")),
     UserDefinedColumn("udc_recommend_position",
       udf(RecommendPositionUtils.getRecommendPosition: (String, String) => String),
       List("path", "pathSub")),
@@ -134,7 +137,10 @@ object Play extends FactEtlBase {
 //    ("singer_or_radio_sid", "udc_singer_or_radio_sid"),
 //    ("mv_hot_key", "udc_mv_hot_key"),
     ("dim_date", "dim_date"),
-    ("dim_time", "dim_time")
+    ("dim_time", "dim_time"),
+    ("udc_launcher_access_area","udc_launcher_access_area"),
+    ("udc_launcher_access_location","udc_launcher_access_location"),
+    ("udc_launcher_location_index","udc_launcher_location_index")
 
   )
 
@@ -166,10 +172,12 @@ object Play extends FactEtlBase {
     new DimensionColumn("dim_whaley_launcher_entrance",
       List(DimensionJoinCondition(
         Map("udc_wui_version" -> "launcher_version",
+          "udc_launcher_access_area" -> "access_area_code",
           "udc_launcher_access_location" -> "access_location_code",
           "udc_launcher_location_index" -> "launcher_location_index")),
         DimensionJoinCondition(
           Map("udc_wui_version" -> "launcher_version",
+            "udc_launcher_access_area" -> "access_area_code",
             "udc_launcher_access_location" -> "access_location_code"),
           "launcher_location_index = -1")
       ), "launcher_entrance_sk"),
