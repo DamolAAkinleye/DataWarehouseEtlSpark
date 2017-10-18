1b45422745887184440f6769c867564ba044a473
张羽 <zhang.yu01@whaley.cn>
2017-10-18 17:58:44 +0800

    ota21新增play日志event过滤逻辑

--- src/main/scala/cn/whaley/datawarehouse/fact/whaley/Play.scala
+++ src/main/scala/cn/whaley/datawarehouse/fact/whaley/Play.scala
@@ -86,7 +86,7 @@ object Play extends FactEtlBase {
     ("trailer_duration", "cast(trailerDuration as bigint)"),
     ("user_id", "userId"),
     ("product_sn", "productSN"),
-    ("end_event", "event "),
+    ("end_event", "event"),
     ("start_time", "cast(startTime as timestamp)"),
     ("end_time", "cast(endTime as timestamp)"),
     ("content_type", "udc_path_content_type"),
